a7bfdce4333063b3ff9bd0c8a9c4264513a01b44
IlyaVatnik <ilya.vatnik@gmail.com>
2026-03-05 11:57:08 +0700

    __version__ = '1.5.1' __date__ = '2026.03.05'

--- GUI_interrogator.py
+++ GUI_interrogator.py
@@ -1,10 +1,10 @@
 
-from UIs.MainWindowInterrogator import MainWindow
+from MainWindow import MainWindow
 from PyQt5 import QtWidgets
 from packaging import version
 import sys
-__version__ = '1.5.0'
-__date__ = '2026.02.27'
+__version__ = '1.5.1'
+__date__ = '2026.03.05'
 
 
 # from conda import __version__ as condaVersion
